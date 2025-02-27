79930fb86723d895c9cbc10e5056638edca43d12
ハイピの廃人 <dada994a@users.noreply.github.com>
2025-02-27 20:18:43 +0900

    leak

---
+++ about OldLib/net/branchlock/layout/references/D.java
@@ -0,0 +1,13 @@
+package net.branchlock.layout.references;
+
+public class D {
+   public static Throwable D;
+
+   public static Throwable D(Throwable var0) {
+      if (D != null) {
+         D = var0;
+      }
+
+      return var0;
+   }
+}
